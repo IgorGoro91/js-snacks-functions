332275b6f6a7810f5d8bdab05e3693c533722174
IgorGoro91 <igorysc@gmail.com>
2025-01-16 18:41:26 +0100

    add:lettere iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,12 +2,21 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
+console.log(names)
 // Dichiara la funzione qui.
 
+function primaLettera( names ) {
+    let iniziali = [];
 
+    for( let i = 0; i < names.length; i++){
+        iniziali.push(names [i] [0]);
+    }
+        return iniziali ;   
+}
+
+primaLettera()
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(primaLettera(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
